ea6cadf48413f31f544a2b5207d88573c3087065
Deividas123 <deividukkas@gmail.com>
2016-12-19 20:58:47 +0200

    Fixing all issues

---
+++ src/js/DeividasP.js
@@ -0,0 +1,12 @@
+/**
+ * Created by HP on 2016.12.16.
+ */
+angular.module('App', []).controller('DeividasPetkevicius', function () {
+    var dp = this;
+
+    dp.kintamasis = "VARDAS";
+
+    dp.par1 = true;
+    dp.masyvas = ['vienas','du','trys','keturi','...'];
+
+})
