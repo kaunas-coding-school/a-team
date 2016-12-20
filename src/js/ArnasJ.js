c8a17bc303e10d9f67b9907b7018a18ef4faa9b8
ArnasJ <hettfield@gmail.com>
2016-12-20 19:31:46 +0200

    Merge branch 'master' of https://github.com/kaunas-coding-school/a-team
    # Conflicts:
    #	Arnas

---
+++ src/js/ArnasJ.js
@@ -0,0 +1,13 @@
+/**
+ * Created by arnas on 2016-12-16.
+ */
+app.controller('ArnasJanulionis', function () {
+
+
+    var aj = this;
+    aj.kint = 'Arnas';
+    aj.vardas = 'Janulionis';
+    aj.par1 = false;
+
+    aj.masyvas = ['vienas', 'du', 'trys', 'keturi'];
+});
